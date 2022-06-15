355b2debf44963b06ce2bb9543c0c7c6fa48abd9
AzadehNazemi <93635916+AzadehNazemi@users.noreply.github.com>
2022-06-15 08:11:05 +0800

    Create warp_crop_4pointTransform.py

---
+++ warp_crop_4pointTransform.py
@@ -0,0 +1,52 @@
+import sys
+import cv2
+import numpy as np
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
+
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+	return rect
+
+def four_point_transform(image, pts):
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+	return warped
+
+image = cv2.imread(sys.argv[1])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+tmp = gray > 0
+coords = np.argwhere(tmp)
+print(coords)
+y0,x0 = coords.min(axis=0)
+y1, x1 = coords.max(axis=0) + 1
+# pts=order_points(coords)
+pts = np.array(([x0,y0],[x1,y0],[x1,y1],[x0,y1]), dtype = "float32")
+
+warped = four_point_transform(image, pts)
+
+cv2.imwrite("Warped.jpg", warped)
